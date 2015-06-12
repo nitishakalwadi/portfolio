3cdba84d82dfe98afd3f92acadf7ceaf70f7c907
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2015-06-12 16:31:44 +0000

    trying out grav cms

---
+++ grav/cache/compiled/files/754813d4e4899d94623ca9fc3ef860ab.yaml.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'system/blueprints/config/streams.yaml',
+    'modified' => 1434125426,
+    'data' => [
+        'title' => 'File Streams',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => [
+                'schemes.xxx' => [
+                    'type' => 'array'
+                ]
+            ]
+        ]
+    ]
+];
---
+++ grav/cache/compiled/files/f8bbd5df972a55a3318d25cabcdb79c4.yaml.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'user/plugins/error/error.yaml',
+    'modified' => 1434125450,
+    'data' => [
+        'enabled' => true,
+        'routes' => [
+            404 => '/error'
+        ]
+    ]
+];
